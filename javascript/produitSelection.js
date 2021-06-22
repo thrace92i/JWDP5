7d9266d6e7ae957df158e145e5c3ea430643c03f
thrace92i <pierre.fournier42@gmail.com>
2021-06-22 17:15:05 +0200

    Création LS avec Popup :+1:

--- javascript/produitSelection.js
+++ javascript/produitSelection.js
@@ -26,7 +26,7 @@ async function idProduitSelectionner() {
         let data = await fetch('http://localhost:3000/api/cameras/' + leId);
         const idCameras = await data.json();
         // console.log(idCameras);
-            affichagePageProduit.innerHTML = `
+        affichagePageProduit.innerHTML = `
                 <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3 m-3">
                     <div class="col-sm-12 col-md-12 col-lg-12">
                         <div class="card shadow-sm">
@@ -55,66 +55,101 @@ async function idProduitSelectionner() {
                     </div>
                 </div>; `;
 
-                //GESTION DU PANIER
-                //Sélection de l'ID du formulaire
-                let idForm = document.querySelector('#lenses_cameras')
-                // console.log(idForm);
+        //GESTION DU PANIER
+        //Sélection de l'ID du formulaire
+        let idForm = document.querySelector('#lenses_cameras')
+        // console.log(idForm);
+
+
+        //Sélection du bouton Ajouter l'article au panier
+
+        let btnPanier = document.querySelector('#btnPanier')
+        // console.log(btnPanier);
+
+        //Ecouter le bouton et envoyer le panier
+        btnPanier.addEventListener("click", (e) => {
+            e.preventDefault;
+
+            //Mettre le choix de l'utilisateur dans une variable
+
+            let choixObjectif = idForm.value;
+
+            //Récupération des valeurs du formulaire
+
+            let optionsCameras = {
+                imageCamera: idCameras.imageUrl,
+                nomCamera: idCameras.name,
+                idCamera: idCameras._id,
+                lensesCamera: choixObjectif,
+                quantite: 1,
+                prixCamera: idCameras.price / 100,
+            }
+            console.log(optionsCameras);
+
+            // -------- LE LOCAL STORAGE ---------
+            // Je stocke les données du formulaire dans le local storage
+
+            //Je décalre une variable "cameraEnregistrerDansLocalStorage"
+
+            let cameraEnregistrerDansLocalStorage = JSON.parse(localStorage.getItem("cameraPanier"));
+            // JSON.parse permets de convertir les données au format JSON en Objet JS dans le LS
+            console.log(cameraEnregistrerDansLocalStorage);
+
+            //Fonction fenêtre popup lorsqu'on ajoute une caméra au panier
+            const popupConfirmation = () => { 
+                if (window.confirm(`${idCameras.name} avec l'objectif ${choixObjectif} a bien été ajouté à votre panier ! Consultez le panier OK ou revenir à l'acceuil ANNULER `)) {
+                    window.location.href = "formulaire.html";
+                }else{
+                    window.location.href = "index.html";
+                }
+            }
                 
+            
 
-                //Sélection du bouton Ajouter l'article au panier
+            //Produit enregistré dans le LS ?
+            if (cameraEnregistrerDansLocalStorage) {
+                cameraEnregistrerDansLocalStorage.push(optionsCameras);
+                localStorage.setItem("cameraPanier", JSON.stringify(cameraEnregistrerDansLocalStorage));
+                console.log(cameraEnregistrerDansLocalStorage);
+                popupConfirmation();
+            }
+            //Pas de produit enregistré dans le LS ?
+            else {
+                cameraEnregistrerDansLocalStorage = []
+                cameraEnregistrerDansLocalStorage.push(optionsCameras);
+                console.log(cameraEnregistrerDansLocalStorage);
+                localStorage.setItem("cameraPanier", JSON.stringify(cameraEnregistrerDansLocalStorage));
+                popupConfirmation();
+            }
 
-                let btnPanier = document.querySelector('#btnPanier')
-                // console.log(btnPanier);
+        })
 
-                //Ecouter le bouton et envoyer le panier
-                btnPanier.addEventListener("click", (e)=>{
-                    e.preventDefault;
 
-                    //Mettre le choix de l'utilisateur dans une variable
+        //Le formulaire s'adapte au nombres d'objectifs possible pour une caméra
+        const quantiteObjectifs = idCameras.lenses;
+        let recupObjectif = [];
 
-                let choixObjectif = idForm.value;
+        //Création boucle for pour afficher les objectifs possibles pour la caméra
 
-                     //Récupération des valeurs du formulaire
-
-                let optionsCameras = {
-                        imageCamera: idCameras.imageUrl,
-                        nomCamera: idCameras.name,
-                        idCamera: idCameras._id,
-                        lensesCamera: choixObjectif,
-                        quantite: 1,
-                        prixCamera: idCameras.price / 100,
-                    }
-                    console.log(optionsCameras);
-                })
-
-                
-                //Le formulaire s'adapte au nombres d'objectifs possible pour une caméra
-                const quantiteObjectifs = idCameras.lenses;
-                let recupObjectif = [];
-
-                //Création boucle for pour afficher les objectifs possibles pour la caméra
-
-                for (let j = 0; j < quantiteObjectifs.length; j++){
-                    recupObjectif = recupObjectif + 
-                    `
+        for (let j = 0; j < quantiteObjectifs.length; j++) {
+            recupObjectif = recupObjectif +
+                `
                     <option value="${j+1}">${quantiteObjectifs[j]}</option>
                     `
-                    // console.log(recupObjectif);
-                }
+            // console.log(recupObjectif);
+        }
 
-                //injection html des objectifs 
-                const affichageObjectif = document.querySelector('#lenses_cameras');
-                affichageObjectif.innerHTML = recupObjectif;
-                // console.log(affichageObjectif);
+        //injection html des objectifs 
+        const affichageObjectif = document.querySelector('#lenses_cameras');
+        affichageObjectif.innerHTML = recupObjectif;
+        // console.log(affichageObjectif);
 
-               
-                
-                
+
 
     } catch (erreur) {
         console.log(erreur);
     }
-    
+
 }
 
 // Cette fonction asynchrone attend le retour de la fonction asynchrone 
